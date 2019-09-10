8431907c8daf8d5a162de9a5e583a3ba1e2a704e
vladimir.pestov <pestovvladimir@gmail.com>
2019-09-10 15:34:50 +0200

    initial implementation of time restriction

--- date/date_test.go
+++ date/date_test.go
@@ -20,48 +20,55 @@ func TestDateParamToEpoch(t *testing.T) {
 	var tests = []struct {
 		input  string
 		output string
+		error  bool
 	}{
-		{"midnight", "00:00 1994-Aug-16"},
-		{"noon", "12:00 1994-Aug-16"},
-		{"teatime", "16:00 1994-Aug-16"},
-		{"tomorrow", "00:00 1994-Aug-17"},
+		{"midnight", "00:00 1994-Aug-16", false},
+		{"noon", "12:00 1994-Aug-16", false},
+		{"teatime", "16:00 1994-Aug-16", false},
+		{"tomorrow", "00:00 1994-Aug-17", false},
 
-		{"noon 08/12/94", "12:00 1994-Aug-12"},
-		{"midnight 20060812", "00:00 2006-Aug-12"},
-		{"noon tomorrow", "12:00 1994-Aug-17"},
+		{"noon 08/12/94", "12:00 1994-Aug-12", false},
+		{"midnight 20060812", "00:00 2006-Aug-12", false},
+		{"noon tomorrow", "12:00 1994-Aug-17", false},
 
-		{"17:04 19940812", "17:04 1994-Aug-12"},
-		{"-1day", "15:30 1994-Aug-15"},
-		{"19940812", "00:00 1994-Aug-12"},
-		{"now", "15:30 1994-Aug-16"},
-		{"hh:mm 19940812", "00:00 1994-Aug-12"},
-		{"12:30:00 19940812", "00:00 1994-Aug-12"},
-		{"12:mm 19940812", "00:00 1994-Aug-12"},
-		{"today", "00:00 1994-Aug-16"},
-		{"yesterday", "00:00 1994-Aug-15"},
-		{"1556201160", "16:06 2019-Apr-25"},
-		{"", defaultTsStr},
-		{"-something", defaultTsStr},
-		{"17:04 19940812 1001", defaultTsStr},
-		{"12:30 08/15/06", "12:30 2006-Aug-15"},
-		{"12:30 08-15-06", defaultTsStr},
-		{"08/15/06 12:30", defaultTsStr},
-		{"+5m", defaultTsStr},
+		{"17:04 19940812", "17:04 1994-Aug-12", false},
+		{"-1day", "15:30 1994-Aug-15", false},
+		{"19940812", "00:00 1994-Aug-12", false},
+		{"now", "15:30 1994-Aug-16", false},
+		{"hh:mm 19940812", "00:00 1994-Aug-12", true},
+		{"12:30:00 19940812", "00:00 1994-Aug-12", true},
+		{"12:mm 19940812", "00:00 1994-Aug-12", true},
+		{"today", "00:00 1994-Aug-16", false},
+		{"yesterday", "00:00 1994-Aug-15", false},
+		{"1556201160", "16:06 2019-Apr-25", false},
+		{"", defaultTsStr, false},
+		{"-something", defaultTsStr, true},
+		{"17:04 19940812 1001", defaultTsStr, true},
+		{"12:30 08/15/06", "12:30 2006-Aug-15", false},
+		{"12:30 08-15-06", defaultTsStr, true},
+		{"08/15/06 12:30", defaultTsStr, true},
+		{"+5m", defaultTsStr, true},
 	}
 
 	defaultTime, _ := time.ParseInLocation(shortForm, defaultTsStr, defaultTimeZone)
 	defaultTs := defaultTime.Unix()
 
 	for _, tt := range tests {
-		got := DateParamToEpoch(tt.input, "Local", defaultTs, defaultTimeZone)
+		got, parseErr := DateParamToEpoch(tt.input, "Local", defaultTs, defaultTimeZone)
 		ts, err := time.ParseInLocation(shortForm, tt.output, defaultTimeZone)
 		if err != nil {
 			panic(fmt.Sprintf("error parsing time: %q: %v", tt.output, err))
 		}
+		if (tt.error && parseErr == nil) || (!tt.error && parseErr != nil) {
+			t.Errorf("dateParamToEpoch(%q, 0)=%v, want error: %v", tt.input, got, tt.error)
+			continue
+		}
 
-		want := int32(ts.Unix())
-		if got != want {
-			t.Errorf("dateParamToEpoch(%q, 0)=%v, want %v", tt.input, got, want)
+		if !tt.error {
+			want := int32(ts.Unix())
+			if got != want {
+				t.Errorf("dateParamToEpoch(%q, 0)=%v, want %v", tt.input, got, want)
+			}
 		}
 	}
 }
