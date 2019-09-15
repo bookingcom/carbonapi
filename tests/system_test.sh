f8dac06ed90faf2c60e97b60f68946f843695641
kostty <kostty.k@gmail.com>
2019-09-15 22:40:45 +0200

    Only print folds when in Travis CI

--- tests/system_test.sh
+++ tests/system_test.sh
@@ -6,18 +6,28 @@
 #
 # Needs to be run from the root directory of the project.
 
+# `begin_fold` and `end_fold` create output folds in a job log in Travis CI
+begin_fold() {
+    if [ "$TRAVIS" == true ]; then
+        echo "travis_fold:start:$1"
+    fi
+    echo -e "\e[33;1m$2\e[0m"
+}
 
+end_fold() {
+    if [ "$TRAVIS" == true ]; then
+        echo "travis_fold:end:$1"
+    fi
+}
 
-echo "travis_fold:start:docker-build"
-echo -e "\e[33;1mBuild docker images\e[0m"
+begin_fold docker-build "Build docker images"
 docker-compose build || { printf ">>> ERROR: Docker build failed"; exit 2; }
-echo "travis_fold:end:docker-build"
+end_fold docker-build
 
-echo "travis_fold:start:docker-up"
-echo -e "\e[33;1mStarting containers\e[0m"
+begin_fold docker-up "Starting containers"
 docker-compose up -d || { printf ">>> ERROR: Launching containers failed"; exit 2; }
 echo # `echo` to work around formatting issues after `docker-compose`
-echo "travis_fold:end:docker-up"
+end_fold docker-up
 
 # This is guestimate for the time when the system goes up and operational.
 # Doing this other ways is much more complex. May need to be increased on slow boxes.
@@ -26,8 +36,7 @@ sleep 5
 METRIC_NAME="some.test.metric"
 TEST_VALUE=123456
 
-echo "travis_fold:start:testing"
-echo -e "\e[33;1mStoring and retrieving datapoints\e[0m"
+begin_fold testing "Storing and retrieving datapoints"
 echo "$METRIC_NAME $TEST_VALUE $(date +%s)" | nc -q 1 localhost 2003 || { printf ">>> ERROR: Putting data in failed"; exit 2; }
 
 # wait to make sure data got there
@@ -41,13 +50,12 @@ else
     res='FAIL!'
     rc=1
 fi
-echo "travis_fold:end:testing"
+end_fold testing
 
 echo ">>> $res"
 
-echo "travis_fold:start:docker-down"
-echo -e "\e[33;1mStopping containers\e[0m"
+begin_fold docker-down "Stopping containers"
 docker-compose down
-echo "travis_fold:end:docker-down"
+end_fold docker-down
 
 exit $rc
