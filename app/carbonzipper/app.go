a0cba398bb239e4bdd19f9fcf7e0c55c7082811b
Andrei Vereha <andrei.vereha@booking.com>
2020-07-29 13:09:56 +0200

    add service.version tag with 'BuildVersion'

--- app/carbonzipper/app.go
+++ app/carbonzipper/app.go
@@ -67,7 +67,7 @@ func New(config cfg.Zipper, logger *zap.Logger, buildVersion string) (*App, erro
 func (app *App) Start() func() {
 	logger := zapwriter.Logger("zipper")
 
-	flush := trace.InitTracer("carbonzipper", logger, app.config.Traces)
+	flush := trace.InitTracer(BuildVersion, "carbonzipper", logger, app.config.Traces)
 
 	types.SetCorruptionWatcher(app.config.CorruptionThreshold, logger)
 
