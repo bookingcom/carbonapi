d9fb5fd7f9de45acaa2b74388a2bc2f546c51de2
Andrei Vereha <avereha@users.noreply.github.com>
2020-08-12 14:22:25 +0200

    Avereha/combine series (#281)
    Similar to #277, fix 'must have the same length " errors for 'seriesLists' functions
    I'm using exactly the same logic as in #277. If two series don't have the same length, consolidate both to the same length.
    Moved this logic to its own function to avoid copy/paste. :)

--- expr/functions/divideSeries/function.go
+++ expr/functions/divideSeries/function.go
@@ -29,6 +29,13 @@ func New(configFile string) []interfaces.FunctionMetadata {
 	return res
 }
 
+func divide(a, b float64) float64 {
+	if b == 0 {
+		return math.NaN()
+	}
+	return a / b
+}
+
 // divideSeries(dividendSeriesList, divisorSeriesList)
 func (f *divideSeries) Do(e parser.Expr, from, until int32, values map[parser.MetricRequest][]*types.MetricData) ([]*types.MetricData, error) {
 	if len(e.Args()) < 1 {
@@ -43,7 +50,7 @@ func (f *divideSeries) Do(e parser.Expr, from, until int32, values map[parser.Me
 	var useMetricNames bool
 
 	var numerators []*types.MetricData
-	var originalDenominator *types.MetricData
+	var denominator *types.MetricData
 	if len(e.Args()) == 2 {
 		useMetricNames = true
 		numerators = firstArg
@@ -55,70 +62,23 @@ func (f *divideSeries) Do(e parser.Expr, from, until int32, values map[parser.Me
 			return nil, types.ErrWildcardNotAllowed
 		}
 
-		originalDenominator = denominators[0]
+		denominator = denominators[0]
 	} else if len(firstArg) == 2 && len(e.Args()) == 1 {
 		numerators = append(numerators, firstArg[0])
-		originalDenominator = firstArg[1]
+		denominator = firstArg[1]
 	} else {
 		return nil, errors.New("must be called with 2 series or a wildcard that matches exactly 2 series")
 	}
 
-	denominator := *originalDenominator
 	var results []*types.MetricData
-	for _, originalNumerator := range numerators {
-		numerator := *originalNumerator
+	for _, numerator := range numerators {
+
 		name := fmt.Sprintf("divideSeries(%s)", e.RawArgs())
 		if useMetricNames {
 			name = fmt.Sprintf("divideSeries(%s,%s)", numerator.Name, denominator.Name)
 		}
 
-		step := helper.LCM(numerator.StepTime, denominator.StepTime)
-
-		numerator.SetValuesPerPoint(int(step / numerator.StepTime))
-		denominator.SetValuesPerPoint(int(step / denominator.StepTime))
-
-		start := numerator.StartTime
-		if start > denominator.StartTime {
-			start = denominator.StartTime
-		}
-		end := numerator.StopTime
-		if end < denominator.StopTime {
-			end = denominator.StopTime
-		}
-		end -= (end - start) % step
-		length := int((end - start) / step)
-
-		numeratorAbsent := numerator.AggregatedAbsent()
-		if len(numeratorAbsent) > length {
-			length = len(numeratorAbsent)
-		}
-		numeratorValues := numerator.AggregatedValues()
-		if len(numeratorValues) > length {
-			length = len(numeratorValues)
-		}
-		denominatorAbsent := denominator.AggregatedAbsent()
-		if len(denominatorAbsent) > length {
-			length = len(denominatorAbsent)
-		}
-		denominatorValues := denominator.AggregatedValues()
-		if len(denominatorValues) > length {
-			length = len(denominatorValues)
-		}
-		values := make([]float64, length)
-		for i := 0; i < length; i++ {
-			if i >= len(numeratorAbsent) || i >= len(denominatorAbsent) ||
-				i >= len(numeratorValues) || i >= len(denominatorValues) {
-				values[i] = math.NaN()
-				continue
-			}
-			if numeratorAbsent[i] || denominatorAbsent[i] || denominatorValues[i] == 0 {
-				values[i] = math.NaN()
-				continue
-			}
-			values[i] = numeratorValues[i] / denominatorValues[i]
-		}
-
-		result := types.MakeMetricData(name, values, step, start)
+		result := helper.CombineSeries(numerator, denominator, name, divide)
 		results = append(results, result)
 	}
 
