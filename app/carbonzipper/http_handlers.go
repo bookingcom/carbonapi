46c9313ec3ffbe211a8da3eea309bb9ea09cc92d
Andrei Vereha <avereha@users.noreply.github.com>
2020-09-23 14:59:14 +0200

    Avereha/zipper ctx (#317)
    * move handler_test to http_handlers_test to match the tested filename
    
    * zipper: group errors by message and count them
    
    That way we have better visibility over the errors received by zipper.
    Limit size of resulting error message.
    
    * limit error message size

--- app/carbonzipper/http_handlers.go
+++ app/carbonzipper/http_handlers.go
@@ -583,6 +583,7 @@ func (app *App) filterByTopLevelDomain(backends []backend.Backend, targetTLDs []
 
 func errorsFanIn(ctx context.Context, errs []error, nBackends int) error {
 	nErrs := len(errs)
+	var counts = make(map[string]int)
 	switch {
 	case (nErrs == 0):
 		return nil
@@ -594,6 +595,7 @@ func errorsFanIn(ctx context.Context, errs []error, nBackends int) error {
 		// everything failed, nErrs == nBackends
 		nNotNotFounds := 0
 		for _, e := range errs {
+			counts[e.Error()] += 1
 			if _, ok := e.(types.ErrNotFound); !ok {
 				nNotNotFounds += 1
 			}
@@ -606,9 +608,10 @@ func errorsFanIn(ctx context.Context, errs []error, nBackends int) error {
 				"majority of backends returned not found. %d total errors, %d not found",
 				nErrs, nErrs-nNotNotFounds))
 		}
-
-		return errors.New(fmt.Sprintf(
-			"all backends failed with mixed errors: %d total errors, %d not found",
-			nErrs, nErrs-nNotNotFounds))
+		message := fmt.Sprintf("all backends failed with mixed errors: %+v", counts)
+		if len(message) > 300 {
+			message = message[:300]
+		}
+		return errors.New(message)
 	}
 }
