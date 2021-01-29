bookingcom/carbonapi

commit 2f9c4cd9d606a65b5cdf15706011f19864f1ad3f

linter: all 'switch' related issues

pkg/parser/parser.go
@@ -52,6 +52,8 @@ func (e *expr) ToString() string {
 		s = strings.Replace(s, `\`, `\\`, -1)
 		s = strings.Replace(s, `'`, `\'`, -1)
 		return "'" + s + "'"
+	case EtName:
+		return fmt.Sprint(e.target)
 	}
 
 	return e.target
@@ -180,6 +182,8 @@ func (e *expr) Metrics() []MetricRequest {
 				for i := range r {
 					r[i].From -= offs
 				}
+			default:
+				return nil
 			}
 		}
 		return r

.golangci.yml
@@ -137,8 +137,10 @@ linters-settings:
       captLocal: # must be valid enabled check name
         paramsOnly: true
       rangeValCopy:
-        sizeThreshold: 32
-
+        sizeThreshold: 32  
+  exhaustive:
+    default-signifies-exhaustive: true
+    
 linters:
   enable:
     - megacheck

commit 953a89ee81da2ac4536640ec22578a1c34e278d9

Fix bug introduced int:"linter: all 'switch' related issues"
This fixes a bug introduced in: 2f9c4cd9d606a65b5cdf15706011f19864f1ad3f.
For the `moving*` functions, default was to return `r` and not `nil`

pkg/parser/parser.go
@@ -52,11 +52,9 @@ func (e *expr) ToString() string {
 		s = strings.Replace(s, `\`, `\\`, -1)
 		s = strings.Replace(s, `'`, `\'`, -1)
 		return "'" + s + "'"
-	case EtName:
-		return fmt.Sprint(e.target)
+	default:
+		return e.target
 	}
-
-	return e.target
 }
 
 func (e *expr) SetTarget(target string) {
@@ -173,8 +171,7 @@ func (e *expr) Metrics() []MetricRequest {
 				r[i].From -= 7 * 86400 // starts -7 days from where the original starts
 			}
 		case "movingAverage", "movingMedian", "movingMin", "movingMax", "movingSum":
-			switch e.args[1].etype {
-			case EtString:
+			if e.args[1].etype == EtString {
 				offs, err := e.GetIntervalArg(1, 1)
 				if err != nil {
 					return nil
@@ -182,8 +179,6 @@ func (e *expr) Metrics() []MetricRequest {
 				for i := range r {
 					r[i].From -= offs
 				}
-			default:
-				return nil
 			}
 		}
 		return r