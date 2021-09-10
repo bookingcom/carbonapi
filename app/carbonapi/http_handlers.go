bookingcom/carbonapi

commit f74343ceb74500b9b4997470604cea6f135054eb

carbonapi/render: continue query exection instead of not found/404
Continue query execution even though no metric is found in
prefetch as there are Graphite query functions that are able
to handle no data and users expect proper result returned. Example:
	fallbackSeries(metric.not.exist, constantLine(1))
Refrence behaviour in graphite-web: https://github.com/graphite-project/graphite-web/blob/1.1.8/webapp/graphite/render/evaluator.py#L14-L46

app/carbonapi/http_handlers.go
@@ -244,7 +244,15 @@ func (app *App) renderHandler(w http.ResponseWriter, r *http.Request) {
 		targetErr, metricSize := app.getTargetData(targetCtx, target, exp, metricMap,
 			form.useCache, form.from32, form.until32, &toLog, logger, &partiallyFailed, targetSpan)
 
-		if targetErr == nil {
+		// Continue query execution even though no metric is found in
+		// prefetch as there are Graphite query functions that are able
+		// to handle no data and users expect proper result returned. Example:
+		//
+		// 	fallbackSeries(metric.not.exist, constantLine(1))
+		//
+		// Refrence behaviour in graphite-web: https://github.com/graphite-project/graphite-web/blob/1.1.8/webapp/graphite/render/evaluator.py#L14-L46
+		var notFound dataTypes.ErrNotFound
+		if targetErr == nil || errors.As(targetErr, &notFound) {
 			targetErr = evalExprRender(targetCtx, exp, &results, metricMap, &form, app.config.PrintErrorStackTrace, getTargetData)
 		}
 		targetSpan.AddEvent(targetCtx, "evaluated expression")
@@ -255,7 +263,6 @@ func (app *App) renderHandler(w http.ResponseWriter, r *http.Request) {
 			// b) parser.ParseError -> Return with this error(like above, but with less details )
 			// c) anything else -> continue, answer will be 5xx if all targets have one error
 			var parseError parser.ParseError
-			var notFound dataTypes.ErrNotFound
 			switch {
 			case errors.As(targetErr, &notFound):
 				// When not found, graphite answers with  http 200 and []
@@ -374,6 +381,7 @@ func (app *App) getTargetData(ctx context.Context, target string, exp parser.Exp
 	metrics := 0
 	var targetMetricFetches []parser.MetricRequest
 	var metricErrs []error
+
 	for _, m := range exp.Metrics() {
 		mfetch := m
 		mfetch.From += from

commit e450430e7ef9b3815d7d4056b565a4d97a8ba207

carbonapi/render: do not return error for parser.ErrSeriesDoesNotExist
This fixes a regression caused by commit f74343ceb74500b9b4997470604cea6f135054eb.
It would be nice to return the error message, but it seems we do not have a way
to communicate it to grafana unless failing all the other queries in the same request.
* https://github.com/grafana/grafana/blob/v7.5.10/pkg/tsdb/graphite/types.go\#L5-L8
* https://github.com/grafana/grafana/blob/v7.5.10/pkg/tsdb/graphite/graphite.go\#L162-L167

app/carbonapi/http_handlers.go
@@ -266,6 +266,20 @@ func (app *App) renderHandler(w http.ResponseWriter, r *http.Request) {
 			switch {
 			case errors.As(targetErr, &notFound):
 				// When not found, graphite answers with  http 200 and []
+			case errors.Is(targetErr, parser.ErrSeriesDoesNotExist):
+				// As now carbonapi continues query execution
+				// when no metrics are returned, it's possible
+				// to have evalExprRender returning this error.
+				// carbonapi should continue executing other
+				// queries in the API call to keep being backward-compatible.
+				//
+				// It would be nice to return the error message,
+				// but it seems we do not have a way to
+				// communicate it to grafana and other users of
+				// carbonapi unless failing all the other queries in the same request:
+				//
+				// * https://github.com/grafana/grafana/blob/v7.5.10/pkg/tsdb/graphite/types.go\#L5-L8
+				// * https://github.com/grafana/grafana/blob/v7.5.10/pkg/tsdb/graphite/graphite.go\#L162-L167
 			case errors.As(targetErr, &parseError):
 				writeError(uuid, r, w, http.StatusBadRequest, targetErr.Error(), form.format, &toLog, span)
 				logAsError = true