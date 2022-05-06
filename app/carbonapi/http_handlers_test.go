f5f58c3d90d43c75e5a82be1209e6fba81af4506
Emad Mohamadi <emadolsky@gmail.com>
2022-05-06 19:43:09 +0200

    Add break globs logic in render handler

--- app/carbonapi/http_handlers_test.go
+++ app/carbonapi/http_handlers_test.go
@@ -127,3 +127,67 @@ func TestOptimistErrsFanIn(t *testing.T) {
 		})
 	}
 }
+
+func TestGetBrokenGlobs(t *testing.T) {
+	var tests = []struct {
+		name       string
+		metric     string
+		glob       *typ.Matches
+		newQueries []string
+	}{
+		{
+			name:   "test1",
+			metric: "a1.*.*.*",
+			glob: &typ.Matches{
+				Name: "a1.*.*.*",
+				Matches: []typ.Match{
+					{Path: "a1.b1.c1.d1", IsLeaf: true}, {Path: "a1.b1.c1.d2", IsLeaf: true}, {Path: "a1.b1.c1.d3", IsLeaf: true},
+					{Path: "a1.b1.c2.d1", IsLeaf: true}, {Path: "a1.b1.c2.d2", IsLeaf: true}, {Path: "a1.b1.c2.d3", IsLeaf: true},
+				},
+			},
+			newQueries: []string{"a1.b1.c1.*", "a1.b1.c2.*"},
+		},
+		{
+			name:   "test2",
+			metric: "a1.b*.c*.d*",
+			glob: &typ.Matches{
+				Name: "a1.b*.c*.d*",
+				Matches: []typ.Match{
+					{Path: "a1.b1.c1.d1", IsLeaf: true}, {Path: "a1.b1.c1.d2", IsLeaf: true}, {Path: "a1.b1.c1.d3", IsLeaf: true},
+					{Path: "a1.b1.c2.d1", IsLeaf: true}, {Path: "a1.b1.c2.d2", IsLeaf: true}, {Path: "a1.b1.c2.d3", IsLeaf: true},
+				},
+			},
+			newQueries: []string{"a1.b1.c1.d*", "a1.b1.c2.d*"},
+		},
+	}
+
+	for _, tst := range tests {
+		tst := tst
+		t.Run(tst.name, func(t *testing.T) {
+			newQueries, _ := getBrokenGlobs(tst.metric, tst.glob)
+			if !sameMatches(newQueries, tst.newQueries) {
+				t.Fatalf("newQueries is different from expected: %+v, %+v", newQueries, tst.newQueries)
+			}
+		})
+	}
+}
+
+func sameMatches(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	diff := make(map[string]int, len(x))
+	for _, _x := range x {
+		diff[_x]++
+	}
+	for _, _y := range y {
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y] -= 1
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}
