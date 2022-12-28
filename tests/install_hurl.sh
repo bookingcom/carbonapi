c56c71b05e7c8b78578fa0f55a92a6df58c4a561
Denys Zhdanov <denys.zhdanov@booking.com>
2022-12-28 19:46:55 +0100

    fix hurl install

--- tests/install_hurl.sh
+++ tests/install_hurl.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     URL="https://github.com/Orange-OpenSource/hurl/releases/download/$VERSION/hurl-$VERSION-x86_64-linux.tar.gz"
     INSTALL_DIR="/var/tmp"
     curl -sL $URL | tar xvz -C $INSTALL_DIR
-    mv -fv $INSTALL_DIR/hurl-1.8.0/hurl /usr/bin && chmod +x /usr/bin/hurl
+    sudo mv -fv $INSTALL_DIR/hurl-1.8.0/hurl /usr/bin && chmod +x /usr/bin/hurl
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install hurl
 fi
