6a6cb61d4453fdbb0e9cb567244d77e512d25631
Denys Zhdanov <denys.zhdanov@booking.com>
2022-12-28 19:25:05 +0100

    migrate e2e tests to hurl

---
+++ tests/install_hurl.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    VERSION="1.8.0"
+    URL="https://github.com/Orange-OpenSource/hurl/releases/download/$VERSION/hurl-$VERSION-x86_64-linux.tar.gz"
+    INSTALL_DIR="/var/tmp"
+    curl -sL $URL | tar xvz -C $INSTALL_DIR
+    mv -fv $INSTALL_DIR/hurl-1.8.0/hurl /usr/bin && chmod +x /usr/bin/hurl
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install hurl
+fi
